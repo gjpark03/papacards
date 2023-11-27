beb8d53ed4e476b8c589c766f49c328f6fb70c8e
Neil Maharishi <maharish@usc.edu>
2023-11-26 21:03:11 -0800

    user backend code

--- team10/src/main/java/com/example/team10/user/User.java
+++ team10/src/main/java/com/example/team10/user/User.java
@@ -1,5 +1,93 @@
 package com.example.team10.user;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
+@Entity
+@Table(name = "User")
 public class User {
-    private Integer userid;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int userID;
+
+    @Column(nullable = false, length = 40)
+    private String username;
+
+    @Column(nullable = false, length = 40)
+    private String email;
+
+    @Column(nullable = false, length = 40)
+    private String password;
+
+    @Column(name = "fName", nullable = false, length = 40)
+    private String firstName;
+
+    @Column(name = "lName", nullable = false, length = 40)
+    private String lastName;
+
+    // constructors, getters, and setters
+
+    public User() {
+    }
+
+    public User(String username, String email, String password, String firstName, String lastName) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    // Getters and Setters
+    public int getUserID() {
+        return userID;
+    }
+
+    public void setUserID(int userID) {
+        this.userID = userID;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
 }
