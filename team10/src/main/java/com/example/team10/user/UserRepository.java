a9f4103784577e760291a00625e8289e8b2da53a
Quoc <qlnguyen@usc.edu>
2023-11-27 21:45:39 -0800

    Update UserRepository.java

--- team10/src/main/java/com/example/team10/user/UserRepository.java
+++ team10/src/main/java/com/example/team10/user/UserRepository.java
@@ -1,6 +1,8 @@
+package com.example.team10.user;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
-public interface UserRepository extends JpaRepository<User, Integer> {
+public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByUsername(String username);
 }
