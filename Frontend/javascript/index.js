82ce4d84dd5c92951a318e3af2ff4ce6e9572a11
Grace Park <gjpark@usc.edu>
2023-11-27 20:58:24 -0800

    Create index.js for displaying flashcard sets on home page

---
+++ Frontend/javascript/index.js
@@ -0,0 +1,24 @@
+document.addEventListener('DOMContentLoaded', () => {
+    loadFlashcards();
+});
+
+// QUERY FLASHCARD SET SQL TABLE AND RETURN RESULT
+function loadFlashcards() {
+    // Fetch the flashcard sets from your backend API
+    fetch('INSERT_BACKEND_SERVLET_HERE')
+    .then(response => response.json())
+    .then(sets => {
+        sets.forEach(set => {
+            document.getElementById('flashcard-sets-container').appendChild(createSetElement(set));
+        });
+    })
+    .catch(error => console.error('Error:', error));
+}
+
+function createSetElement(setData) {
+    const setElement = document.createElement('div');
+    setElement.className = 'flashcard-set';
+    setElement.textContent = setData.name;
+    setElement.onclick = () => window.location.href = `/studydeckloggedin.html?setID=${setData.setID}`;
+    return setElement;
+}
